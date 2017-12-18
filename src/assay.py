33ec044850606e54f81d1b37efb5d51edc4f89c9
Laura Hughes <flaneuse@users.noreply.github.com>
2017-12-18 11:42:56 -0800

    testing looping

--- src/assay.py
+++ src/assay.py
@@ -10,12 +10,42 @@ import pandas as pd
 data_file = 'data/assay_descriptions.csv'
 df = pd.read_csv(data_file)
 
+# print(df)
+# for idx, row in df.iterrows():
+#     print(df.at[idx, 'assay_name'])
 
 # Create Flask app
 app = Flask(__name__) # Use __name__ for single app; __main__ for multiple mods
 
-@app.route('/assays/')
+@app.route('/')
+def make_homepage():
+    return render_template('all_assays.html', df = df)
 
+#
+# @app.route('/<int:row_id>')
+# def make_indiv(row_id):
+#     # print()
+#     # for row in df:
+#     for idx, row in df.iterrows():
+#         print("starting for loop")
+#         print(idx)
+#         # print(row)
+#         # print('row([id]): ')
+#         # print(row['id'])
+#         # print(type(row['id']))
+#         # print('input: ')
+#         # print(row_id)
+#         # print(type(row_id))
+#         # print('logic:')
+#         # print(row['id'] == row_id)
+#         # if row['id'] == row_id:
+#         # print("LOGIC IS TRU")
+#         # return render_template(template, object=row)
+#         # for i in range(5): # Doesn't work outside make_homepage; need to not override make_homepage each time.
+#             # return render_template('test.html', id = row)
+#         return "hello world."
+
+@app.route('/assays/')
 def make_assays(row_id = 0):
 
     # temporarily, and not particularly intelligently: pull out just the first row as a test case.
@@ -59,3 +89,6 @@ def make_assays(row_id = 0):
     }
 
     return render_template('assay.html', df = assay_df)
+
+# if __name__ == '__main__':
+#     app.run(debug=True, use_reloader=True)
